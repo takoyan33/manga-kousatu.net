9d1786383e3b710372da323e4c861282f31b9593
takoyan33 <harrier2070@gmail.com>
2022-11-23 00:41:00 +0900

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,5 @@
-import Head from "next/head";
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import ClipLoader from "react-spinners/ClipLoader";
-import { textAlign } from "@mui/system";
 import Footer from "../layouts/components/Footer";
 
 function MyApp({ Component, pageProps }) {

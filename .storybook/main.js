526e47e2ca04b86c55a98baabeb5e15233863abe
takoyan33 <harrier2070@gmail.com>
2022-12-08 14:07:12 +0900

    storybook動いた

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,10 +6,19 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    }
   ],
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-webpack5"
   }
+  
 }

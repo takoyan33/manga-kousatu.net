9bc39681af2b33ae4e048fcaa676d9a2834a6bd9
takoyan33 <harrier2070@gmail.com>
2022-12-30 12:15:21 +0900

    登録ログインロジック分離

--- pages/profile/[userid].jsx
+++ pages/profile/[userid].jsx
@@ -3,41 +3,51 @@ import React from "react";
 import { useEffect, useState, useCallback } from "react";
 import { database } from "../../firebaseConfig";
 import { collection, getDocs } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 import { MuiNavbar } from "../../layouts/components/MuiNavbar";
 import { SiteHead } from "../../layouts/components/ui/SiteHead";
 import { Profileid } from "../../layouts/components/ui/Profileid";
 import Grid from "@material-ui/core/Grid";
 import { Cardpost } from "../../layouts/Cardpost";
-import { query, where } from "firebase/firestore";
-import { useAuthContext } from "../../layouts/context/AuthContext";
+import { query, orderBy, where } from "firebase/firestore";
+
 const Post = () => {
   const [users, setUsers] = useState(null);
   const databaseRef = collection(database, "posts");
   //データベースを取得
   const [firedata, setFiredata] = useState([]);
   const usersRef = collection(database, "users");
-  const [Userid, setUserid] = useState(null);
-  const [greeting, setGreeting] = useState("hello");
-  const [str, setStr] = useState("");
+  const [likes, setLikes] = useState(null);
   const router = useRouter();
   const { userid } = router.query;
+  const auth = getAuth();
+  const user = auth.currentUser;
+  const yourprofile = query(usersRef, where("userid", "==", userid));
   console.log(yourprofile);
   console.log({ userid });
 
-  const { user } = useAuthContext();
-  const yourprofile = query(usersRef, where("userid", "==", userid));
-  const yourdata = query(databaseRef, where("userid", "==", userid));
-
   const getallPost = async () => {
     //firestoreからデータ取得
-    await getDocs(yourdata).then((response) => {
+    await getDocs(databaseRef).then((response) => {
       //コレクションのドキュメントを取得
       setFiredata(
-        response.docs.map((data) => {
-          //配列なので、mapで展開する
-          return { ...data.data(), id: data.id };
-          //スプレッド構文で展開して、新しい配列を作成
-        })
+        response.docs
+          .map((data) => {
+            //配列なので、mapで展開する
+            return { ...data.data(), id: data.id };
+            //スプレッド構文で展開して、新しい配列を作成
+          })
+          .filter((data) => {
+            if (data.userid === userid) {
+              return data;
+              //そのまま返す
+            } else if (
+              data.userid.toLowerCase().includes(userid)
+              //valのnameが含んでいたら小文字で返す　含んでいないvalは返さない
+            ) {
+              return data;
+            }
+          })
       );
     });
   };
@@ -46,19 +56,31 @@ const Post = () => {
     if (user) {
       if (userid == user.uid) {
         router.push("/profile");
-      } else {
-        userData();
-        getallPost();
-        setUserid({ userid });
-        console.log(Userid);
       }
     }
-    if (str === "goodbye") {
-      const newGreeting = str;
-      setGreeting(newGreeting);
-    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [str]);
+  }, []);
+
+  useEffect(() => {
+    userData();
+    getallPost();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [likes]);
+
+  // const userData = async () => {
+  //   //firestoreからデータ取得
+  //   const data = doc(database, "users", "ilvobFpZAceNGncxNWwPOE88kvu1");
+  //   // const data = doc(database, "users", userid);
+
+  //   getDoc(data)
+  //     .then((documentSnapshot) => {
+  //       setUsers(documentSnapshot.data());
+  //     })
+  //     .catch((error) => {
+  //       console.log("Error getting document:", error);
+  //     });
+  // };
+  // console.log(users);
 
   const userData = async () => {
     //firestoreからデータ取得
@@ -85,13 +107,15 @@ const Post = () => {
             users.map((user) => {
               return (
                 <>
-                  <Profileid
-                    key={user.id}
-                    profileimage={user.profileimage}
-                    username={user.username}
-                    bio={user.bio}
-                    favarite={user.favarite}
-                  />
+                  {userid == user.userid && (
+                    <Profileid
+                      key={user.id}
+                      profileimage={user.profileimage}
+                      username={user.username}
+                      bio={user.bio}
+                      favarite={user.favarite}
+                    />
+                  )}
                 </>
               );
             })}

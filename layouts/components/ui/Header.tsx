27bfdffa3464e9a2f8da55e48905e7dda8662f17
abeshumpei <harrier2070@gmail.com>
2024-07-14 09:04:12 +0900

    loginのスタイル修正

--- layouts/components/ui/Header.tsx
+++ layouts/components/ui/Header.tsx
@@ -44,7 +44,7 @@ const LOGIN_ADMIN_MENU_ITEMS = [
 
 export const Header = () => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
-  const user: any = useAuthContext()
+  const { user } = useAuthContext()
   const router = useRouter()
   const { logout } = useLogOut()
   const [notificationOpen, setNotificationOpen] = useState(false)

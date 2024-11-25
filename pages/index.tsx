dd48ec5d115868bcee7ce033c11d8d0f64279076
abeshumpei <harrier2070@gmail.com>
2024-11-25 20:11:09 +0900

    fix: slideショーのボタンの追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,46 +51,75 @@ export default function Index() {
     <div className='m-auto w-11/12 md:w-full'>
       <CommonHead />
       <TopTitle title='おすすめ記事' url='/top/recommend' />
-      <Swiper
-        className='m-auto flex flex-col flex-wrap justify-center md:flex-row'
-        spaceBetween={10}
-        slidesPerView={3.5}
-        breakpoints={breakpoints}
-        modules={[Navigation]}
-        navigation={{
-          nextEl: '.next-button-recommend',
-          prevEl: '.prev-button-recommend',
-        }}
-      >
-        {oldPostData.length === 0 ? (
-          <p className='my-2 text-center'>記事がありません。</p>
-        ) : (
-          oldPostData.map((post) => (
-            <SwiperSlide key={post.id}>
-              <CardPost
-                downloadURL={post.downloadURL}
-                title={post.title}
-                category={post.category}
-                netabare={post.netabare}
-                context={post.context}
-                createTime={post.createTime}
-                displayName={post.displayName}
-                email={post.email}
-                id={post.id}
-                photoURL={post.photoURL}
-                likes={post.likes}
-                selected={post.selected}
-                userid={post.userid}
-              />
-            </SwiperSlide>
-          ))
-        )}
-      </Swiper>
+      <div className='m-auto flex items-center justify-center md:flex-row'>
+        <div className='prev-button-recommend mr-0 w-48 cursor-pointer md:mr-4 md:w-20'>
+          <Image
+            src='/images/prev-arrow.svg'
+            width={20}
+            height={20}
+            style={{
+              width: '100%',
+              height: 'auto',
+            }}
+            className='cursor-pointer'
+            alt='スライドショーのナビゲーション'
+          />
+        </div>
+        <Swiper
+          className='m-auto flex flex-col flex-wrap justify-center md:flex-row'
+          spaceBetween={10}
+          slidesPerView={3.5}
+          breakpoints={breakpoints}
+          modules={[Navigation]}
+          navigation={{
+            nextEl: '.next-button-recommend',
+            prevEl: '.prev-button-recommend',
+          }}
+        >
+          {oldPostData.length === 0 ? (
+            <p className='my-2 text-center'>記事がありません。</p>
+          ) : (
+            oldPostData.map((post) => (
+              <SwiperSlide key={post.id}>
+                <CardPost
+                  downloadURL={post.downloadURL}
+                  title={post.title}
+                  category={post.category}
+                  netabare={post.netabare}
+                  context={post.context}
+                  createTime={post.createTime}
+                  displayName={post.displayName}
+                  email={post.email}
+                  id={post.id}
+                  photoURL={post.photoURL}
+                  likes={post.likes}
+                  selected={post.selected}
+                  userid={post.userid}
+                />
+              </SwiperSlide>
+            ))
+          )}
+        </Swiper>
+
+        <div className='next-button-recommend w-48 cursor-pointer md:ml-4 md:w-20'>
+          <Image
+            src='/images/next-arrow.svg'
+            width={20}
+            height={20}
+            style={{
+              width: '100%',
+              height: 'auto',
+            }}
+            className='cursor-pointer'
+            alt='スライドショーのナビゲーション'
+          />
+        </div>
+      </div>
 
       <TopTitle title='新着記事' url='/top/new' />
 
       <div className='m-auto flex items-center justify-center md:flex-row'>
-        <div className='prev-button w-20 cursor-pointer'>
+        <div className='prev-button mr-4 w-48 cursor-pointer md:w-20'>
           <Image
             src='/images/prev-arrow.svg'
             width={20}
@@ -146,7 +175,7 @@ export default function Index() {
           )}
         </Swiper>
 
-        <div className='next-button w-20 cursor-pointer'>
+        <div className='next-button w-48 cursor-pointer md:ml-4 md:w-20'>
           <Image
             src='/images/next-arrow.svg'
             width={20}
@@ -162,41 +191,70 @@ export default function Index() {
       </div>
 
       <TopTitle title='投稿一覧' url='/top/all' />
-      <Swiper
-        className='m-auto flex flex-col flex-wrap justify-center  md:flex-row'
-        spaceBetween={10}
-        slidesPerView={3.5}
-        modules={[Navigation]}
-        breakpoints={breakpoints}
-        navigation={{
-          nextEl: '.next-button-all',
-          prevEl: '.prev-button-all',
-        }}
-      >
-        {postData.length === 0 ? (
-          <p className='my-2 text-center'>記事がありません。</p>
-        ) : (
-          postData.map((post) => (
-            <SwiperSlide key={post.id}>
-              <CardPost
-                downloadURL={post.downloadURL}
-                title={post.title}
-                category={post.category}
-                netabare={post.netabare}
-                context={post.context}
-                createTime={post.createTime}
-                displayName={post.displayName}
-                email={post.email}
-                id={post.id}
-                photoURL={post.photoURL}
-                likes={post.likes}
-                selected={post.selected}
-                userid={post.userid}
-              />
-            </SwiperSlide>
-          ))
-        )}
-      </Swiper>
+      <div className='m-auto flex items-center justify-center md:flex-row'>
+        <div className='prev-button-all mr-4 w-48 cursor-pointer md:w-20'>
+          <Image
+            src='/images/prev-arrow.svg'
+            width={20}
+            height={20}
+            style={{
+              width: '100%',
+              height: 'auto',
+            }}
+            className='cursor-pointer'
+            alt='スライドショーのナビゲーション'
+          />
+        </div>
+        <Swiper
+          className='m-auto flex flex-col flex-wrap justify-center  md:flex-row'
+          spaceBetween={10}
+          slidesPerView={3.5}
+          modules={[Navigation]}
+          breakpoints={breakpoints}
+          navigation={{
+            nextEl: '.next-button-all',
+            prevEl: '.prev-button-all',
+          }}
+        >
+          {postData.length === 0 ? (
+            <p className='my-2 text-center'>記事がありません。</p>
+          ) : (
+            postData.map((post) => (
+              <SwiperSlide key={post.id}>
+                <CardPost
+                  downloadURL={post.downloadURL}
+                  title={post.title}
+                  category={post.category}
+                  netabare={post.netabare}
+                  context={post.context}
+                  createTime={post.createTime}
+                  displayName={post.displayName}
+                  email={post.email}
+                  id={post.id}
+                  photoURL={post.photoURL}
+                  likes={post.likes}
+                  selected={post.selected}
+                  userid={post.userid}
+                />
+              </SwiperSlide>
+            ))
+          )}
+        </Swiper>
+
+        <div className='next-button-all w-48 cursor-pointer md:ml-4 md:w-20'>
+          <Image
+            src='/images/next-arrow.svg'
+            width={20}
+            height={20}
+            style={{
+              width: '100%',
+              height: 'auto',
+            }}
+            className='cursor-pointer'
+            alt='スライドショーのナビゲーション'
+          />
+        </div>
+      </div>
       <TopTitle title='カテゴリで探す' />
       {POST_CATEGORIES.map((category) => {
         const CategoriesInfo = {

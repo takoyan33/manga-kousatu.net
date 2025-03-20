82b62ebbf6d5431f48dfc813ad5ed5709b718c7b
abeshumpei <harrier2070@gmail.com>
2025-03-20 23:25:19 +0900

    update: バンドルサイズの調整

--- next.config.js
+++ next.config.js
@@ -21,3 +21,9 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer(nextConfig)

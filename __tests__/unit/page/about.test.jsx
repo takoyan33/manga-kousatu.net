234e08c2287811b30ca28dffd4a54708eb40ad5b
abeshumpei <harrier2070@gmail.com>
2025-03-22 22:51:58 +0900

    update: testファイルをtxtに

--- __tests__/unit/page/about.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-import About from '../../../pages/about/index'
-import '@testing-library/jest-dom'
-
-describe('A2_Page > About', () => {
-  it('[A2_1_1] レンダリングされているか', () => {
-    render(<About />)
-  })
-})
